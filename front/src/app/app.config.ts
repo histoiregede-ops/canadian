ce8621967c15d758d34aafae505b7e66cc02e09c
histoiregede-ops <histoiregede-ops@users.noreply.github.com>
2026-05-22 16:26:40 +0000

    fix: remove hash routing, add static.json for Render SPA routing

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, LOCALE_ID } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
@@ -14,7 +14,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     { provide: LOCALE_ID, useValue: 'fr' },
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    provideRouter(routes, withHashLocation()),
+    provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi())
   ]
 };
