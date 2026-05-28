e020408cbee9e7d7a6a5bb4bb8277e926e3341e2
histoiregede-ops <histoiregede-ops@users.noreply.github.com>
2026-05-28 16:13:50 +0000

    Fix double-click navigation: close sidebar on NavigationEnd instead of (click)

--- front/src/app/components/sidebar/sidebar.component.ts
+++ front/src/app/components/sidebar/sidebar.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Router } from '@angular/router';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
 import { AuthService } from '../../services/auth';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,12 +11,13 @@ import { AuthService } from '../../services/auth';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
   @Input() isOpen = false;
   @Input() isCollapsed = false;
   @Output() closeSidebar = new EventEmitter<void>();
 
   user: any = null;
+  private sub: Subscription;
 
   constructor(
     private authService: AuthService,
@@ -24,10 +26,15 @@ export class SidebarComponent implements OnInit {
 
   ngOnInit(): void {
     this.user = this.authService.getUser();
+    this.sub = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.closeSidebar.emit();
+    });
   }
 
-  onNavClick(): void {
-    this.closeSidebar.emit();
+  ngOnDestroy(): void {
+    if (this.sub) this.sub.unsubscribe();
   }
 
   logout(): void {
